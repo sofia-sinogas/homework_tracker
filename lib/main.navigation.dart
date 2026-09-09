09658b89785725deb9831c485fd3117dfcb66bd2
Sofia Sinogas <sinog002@umn.edu>
2026-09-09 14:49:53 -0500

    added main.navigation.dart file and added navigation to assignment_list_screen.dart

---
+++ lib/main.navigation.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+import 'assignment_list_screen.dart';
+
+class MainNavigationScreen extends StatefulWidget {
+  const MainNavigationScreen({super.key});
+
+  @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  // Two screens: Home and Assignments
+  final List<Widget> _screens = [
+    const HomeScreen(),
+    const AssignmentListScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ), // BottomNavigationBarItem
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Assignments',
+          ), // BottomNavigationBarItem
+        ],
+      ), // BottomNavigationBar
+    ); // Scaffold
+  }
+}
